9bf1dc2081c1ab4f6e842945ff48245947f14123
sinsael <101236411+sinsael@users.noreply.github.com>
2025-12-18 15:43:35 +0900

    12-18

---
+++ My project (1)/Assets/Scripts/Item/Item_DashSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Item_DashSO : ItemDataSO
+{
+    public override void Effect(BallItemSystem user)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ My project (1)/Assets/Scripts/Item/Item_ScoreSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Item_ScoreSO : ItemDataSO
+{
+
+    public override void Effect(BallItemSystem user)
+    {
+        throw new System.NotImplementedException();
+    }
+}
