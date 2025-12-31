3539446553eaa1e45a08830203b5e68bb257971d
sinsael <101236411+sinsael@users.noreply.github.com>
2025-12-31 23:12:02 +0900

    12-31

---
+++ My project (1)/Assets/Scripts/Trap/PushBlock.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class MoveTile : MonoBehaviour
+{
+    Rigidbody rb;
+    public Vector3 Move;
+    public string playerTag = "Player";
+    public float moveTime = 0.5f;
+    public float delayTimer = 0.2f;
+
+    Vector3 defaultVector;
+    Vector3 targetVector;
+
+    bool isActive = false;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+
+        defaultVector = transform.position;
+        targetVector = defaultVector + Move;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        TryActivateTrap(collision);
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        TryActivateTrap(collision);
+    }
+
+    private void TryActivateTrap(Collider collision)
+    {
+        // 1. �۵� ���̸� ��� ����
+        if (isActive || !collision.CompareTag(playerTag)) return;
+        isActive = true;
+
+        Sequence sequence = DOTween.Sequence();
+
+        sequence.Append(rb.DOMove(targetVector, moveTime).SetEase(Ease.OutQuad));
+        sequence.AppendInterval(delayTimer);
+        sequence.Append(rb.DOMove(defaultVector, moveTime).SetEase(Ease.OutQuad));
+
+        sequence.OnComplete(() => {
+            isActive = false;
+        });
+    }
+
+    private void OnDisable()
+    {
+        transform.DOKill();
+        isActive = false;
+    }
+}
